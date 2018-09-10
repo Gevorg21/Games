583bbd4955d57a552072eadf7ceb673010c82143
Gevorg <40454830+Gevorg21@users.noreply.github.com>
2018-09-10 19:10:48 +0400

    Update Program.cs

--- Casino/Program.cs
+++ Casino/Program.cs
@@ -1,31 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace Casino
+namespace CasinoGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-         
-
+            
+            
             Player player = new Player();
 
             Bot bot = new Bot();
 
             Game g = new Game(player, bot);
+           
 
             g.CheckName();
-            
-            Console.Write("Enter Money: ");
-            player.Money = int.Parse(Console.ReadLine());
-            
+
+
+            Console.Write("Enter money: ");
+            int money=0;
+            player.Money = g.Init(money);
+
             g.Gamee();
             
+
         }
     }
 }
